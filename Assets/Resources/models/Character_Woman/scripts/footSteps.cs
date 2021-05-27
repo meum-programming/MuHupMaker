6d259f2c7c470a489267e9d5e72461ab88b7344a
meum-programming <2master.meum@gmail.com>
2021-05-28 00:26:57 +0900

    100메가 이하 파일 커밋2

---
+++ Assets/Resources/models/Character_Woman/scripts/footSteps.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class footSteps : MonoBehaviour {
+    public AudioClip footStp1;
+    public AudioClip footStp2;
+    public AudioSource audioSopurce;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void footStep1()
+    {
+        audioSopurce.PlayOneShot(footStp1);
+	}
+
+	public void footStep2()
+	{
+        audioSopurce.PlayOneShot(footStp2);
+	}
+}
+
